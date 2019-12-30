c12392a61e4bf1cd8a815897d09ccfa5f122c25e
Patrik <patriknilsson1992@gmail.com>
2019-12-30 20:51:24 +0100

    can now filter products on category

---
+++ resources/views/pages/categories.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.horizontal-master')
+@section('main-content')
+<div class="breadcrumb">
+    <h1>Products</h1>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li>{{$category->name}}</li>
+    </ul>
+</div>
+<div class="separator-breadcrumb border-top"></div>
+<section class="product-cart">
+    <div class="row list-grid">
+        @foreach ($products as $product)
+        <div class="list-item col-md-4 d-flex align-items-stretch">
+            <div class="card o-hidden mb-4 ">
+                <div class="">
+                    <img alt="" src="{{ asset($product->featured_image) }}" />
+                </div>
+                <div class="flex-grow-1 d-bock">
+                    <div class="card-body align-self-center d-flex flex-column justify-content-between align-items-lg-center">
+                        <a class="w-40 w-sm-100" href="">
+                            <div class="item-title">
+                                {{$product->name}}
+                            </div>
+                        </a>
+                        <!-- <p class="m-0 text-muted text-small w-15 w-sm-100">
+                            Gadget
+                        </p> -->
+                        <p class="m-0 text-muted text-small w-15 w-sm-100">
+                            <!-- $32.00 <del class="text-secondary">$54.00</del> -->
+                            {{$product->price}}
+                        </p>
+                        <!-- <p class="m-0 text-muted text-small w-15 w-sm-100 d-none d-lg-block item-badges">
+                            <span class="badge badge-info">20% off</span>
+                        </p> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</section>
+{{ $products->links() }}
+@endsection
