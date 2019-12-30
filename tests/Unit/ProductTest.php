de768fdbe59dec511364bb4f3ff147308c7899df
Patrik <patriknilsson1992@gmail.com>
2019-12-30 14:25:08 +0100

    add test for categories relationship

--- tests/Unit/ProductTest.php
+++ tests/Unit/ProductTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit;
 
 use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Database\Eloquent\Collection;
 use Tests\TestCase;
 
 class ProductTest extends TestCase
@@ -17,5 +18,14 @@ class ProductTest extends TestCase
     {  
         $product = Factory(Product::class)->create(['name' => 'Some project']);
         $this->assertEquals('some-project', $product->slug);
+    }
+
+    /**
+     * @test
+     */
+    public function has_categories()
+    {  
+       $project = Factory(Product::class)->create();
+       $this->assertInstanceOf(Collection::class, $project->categories);
     }
 }
