0104afa1839d337e93b539d08ea151cd06376141
Patrik <patriknilsson1992@gmail.com>
2020-02-25 04:51:05 +0100

    init cart state

--- resources/laravel/js/app.js
+++ resources/laravel/js/app.js
@@ -1,16 +1,15 @@
 import "./bootstrap";
-import store from "./store";
 import Vue from "vue";
 import AddToCart from "./components/AddToCart.vue";
 import CartIcon from "./components/CartIcon.vue";
-
+import {store} from "./store";
 
 Vue.component('add-to-cart', AddToCart)
 Vue.component('cart-icon', CartIcon)
 
-
-
 const app = new Vue({
     el: '#app',
-    store
+    created() {
+        store.addCart(shoppingCart);
+    }
 });
